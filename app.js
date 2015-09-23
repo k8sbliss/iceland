b483a21dc5415fdf90a79bb7d61dfd13725d254c
Kate <kateharkness@outlook.com>
2015-09-23 16:30:06 -0600

    adds ng-table functionlity

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-var app = angular.module("app",[]);
-
-app.controller("main", ["$http", "$scope", function($http, $scope) {  
-
-$http.get('http://apis.is/flight?language=en&type=departures').then(function(flights) {
-      $scope.dep = flights.data.results;
-    }, function(flights) {
-      $scope.dep = flights;
-});
-  
-$http.get('http://apis.is/flight?language=en&type=arrivals').then(function(flights) {
-      $scope.arv = flights.data.results;
-    }, function(flights) {
-      $scope.arv = flights;
-});
-
+var app = angular.module('app', ['ngTable']);
+app.controller('main', ['$scope', '$http','ngTableParams' ,
+    function ($scope, $http, ngTableParams) {
+    tableDataDep = [];
+    tableDataArv = [];
+    //Table configuration
+    $scope.tableParamsD = new ngTableParams({
+        page: 1,
+        count: 5
+    },{
+        total:tableDataDep.length,
+        //Returns the data for rendering
+        getData : function($defer,params){
+            $http.get('http://apis.is/flight?language=en&type=departures').then(function(response) {
+                tableDataDep = response.data.results;
+                $defer.resolve(tableDataDep.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                params.total(tableDataDep.length);
+            });
+        }
+    });
+   $scope.tableParamsA = new ngTableParams({
+        page: 1,
+        count: 5
+    },{
+        total:tableDataArv.length,
+        //Returns the data for rendering
+        getData : function($defer,params){
+            $http.get('http://apis.is/flight?language=en&type=arrivals').then(function(response) {
+                tableDataArv = response.data.results;
+                $defer.resolve(tableDataArv.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                params.total(tableDataArv.length);
+            });
+        }
+    });
 }]);
